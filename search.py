c035b7d4288a8785bdc7de86acf324b1acd392aa
varepsilon123 <chungtszfung2@gmail.com>
2025-02-03 22:10:05 -0800

    feat: enhance search functionality with schema declaration and improved query handling

--- search.py
+++ search.py
@@ -1,15 +1,23 @@
 import tantivy
 import os
 import time
-from tantivy import Query, Occur, SnippetGenerator
+from tantivy import Index, Query, Occur, SchemaBuilder, SnippetGenerator
 
 class Searcher:
     def __init__(self):
         self.project_root = os.path.dirname(__file__)
         self.index_path = os.path.join(self.project_root, 'tantivy_index')
-        self.index = tantivy.Index.open(self.index_path)
+        self.index = Index.open(self.index_path)
         self.searcher = self.index.searcher()
         self.cached_queries = {}
+
+        # Declaring our schema.
+        self.schema_builder = SchemaBuilder()
+        self.schema_builder.add_text_field("title", stored=True)
+        self.schema_builder.add_text_field("content", stored=True)
+        self.schema_builder.add_text_field("url", stored=True)
+        self.schema = self.schema_builder.build()
+
 
     def search(self, query_str, top_k=10):
         start_time = time.time()
@@ -18,29 +26,28 @@ class Searcher:
         if query_str in self.cached_queries:
             complex_query = self.cached_queries[query_str]
         else:
-            complex_query = Query.boolean_query(
-                [
-                    (
-                        Occur.Must,
-                        Query.disjunction_max_query(
-                            [
-                                Query.boost_query(
-                                    self.index.parse_query(query_str, ["title"]), 
-                                    3.0
-                                ),
-                                Query.boost_query(
-                                    self.index.parse_query(query_str, ["content"]), 
-                                    2.0
-                                ),
-                                Query.boost_query(
-                                    self.index.parse_query(query_str, ["url"]), 
-                                    0.5 
-                                ),
-                            ],
-                            0.4,
+            terms = query_str.split()
+            must_queries = [
+                Query.disjunction_max_query(
+                    [
+                        Query.boost_query(
+                            self.index.parse_query(term, ["title"]), 
+                            1.5
                         ),
-                    )
-                ]
+                        Query.boost_query(
+                            self.index.parse_query(term, ["content"]), 
+                            2.0
+                        ),
+                        Query.boost_query(
+                            self.index.parse_query(term, ["url"]), 
+                            0.5 
+                        ),
+                    ],
+                    0.4,
+                ) for term in terms
+            ]
+            complex_query = Query.boolean_query(
+                [(Occur.Must, must_query) for must_query in must_queries]
             )
             self.cached_queries[query_str] = complex_query
         
@@ -51,7 +58,7 @@ class Searcher:
         results = []
         if top_docs:
             snippet_generator = SnippetGenerator.create(
-                self.searcher, complex_query, self.index.schema(), "content"
+                self.searcher, complex_query, self.schema, "content"
             )
             for score, doc_address in top_docs:
                 doc = self.searcher.doc(doc_address)
