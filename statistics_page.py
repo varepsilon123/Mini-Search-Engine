a325ad3a4487e07c29b2e8fd57a743bf08994992
varepsilon123 <chungtszfung2@gmail.com>
2025-02-04 23:03:20 -0800

    feat: add functions to calculate average page size

--- statistics_page.py
+++ statistics_page.py
@@ -29,5 +29,24 @@ def get_indexed_pages_per_domain():
 
     return results
 
+# average page size
+def get_average_page_size():
+    project_root = os.path.dirname(__file__)
+    index_path = os.path.join(project_root, 'tantivy_index')
+    index = tantivy.Index.open(index_path)
+    searcher = index.searcher()
+
+    total_size = 0
+    total_docs = searcher.doc_count()
+
+    for doc_id in range(total_docs):
+        doc = searcher.doc(doc_id)
+        total_size += len(str(doc))
+
+    average_size = total_size / total_docs if total_docs > 0 else 0
+    return average_size
+
+# crawl time
+
 if __name__ == "__main__":
     get_indexed_pages_per_domain()
